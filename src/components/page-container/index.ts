c46d3edaf999600ef8ffe7d6538d4ffead4990c9
Donald Lee <donaldturinglee@gmail.com>
2026-08-09 02:28:11 -0700

    refactor: remove PageContainer component and related files; update imports and styles accordingly

--- src/components/page-container/index.ts
+++
@@ -1,9 +0,0 @@
-import PageContainerBase from "./PageContainer";
-import PageContainerRegion from "./PageContainerRegion";
-
-export const PageContainer = Object.assign(PageContainerBase, {
-    Region: PageContainerRegion,
-});
-
-export { PageContainerRegion };
-export * from "./PageContainer.types";
